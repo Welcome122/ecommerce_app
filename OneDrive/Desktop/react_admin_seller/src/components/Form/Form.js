c27a4b3d5139a709c34d965478acdc2bf5d78936
Welcome122 <karannsingh2421@gmail.com>
2023-04-02 19:22:55 +0530

    project

---
+++ OneDrive/Desktop/react_admin_seller/src/components/Form/Form.js
@@ -0,0 +1,16 @@
+import React from "react";
+ 
+function Form (){
+     return (
+        <form>
+            <label>product id</label>
+            <input type="text"></input>
+            <label>product price</label>
+            <input type="text"></input>
+            <label>product name</label>
+            <input type="text"></input>
+        </form>
+     );
+};
+
+export default Form;
